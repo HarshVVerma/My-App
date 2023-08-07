3da46fe2a009ad4e35e1edabd76528c1c4559cf1
HarshVVerma <harshvardhan.verma@buzzclan.com>
2023-08-07 17:57:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import "./App.css";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar title = "My App" aboutText= "About Us" />
+
+      {/* <Navbar/> */}
+    </>
+  );
+}
+
+export default App;
